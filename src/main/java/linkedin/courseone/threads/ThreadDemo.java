69f6553efff74da584b83b65fa3971458ffa2aac
Sipho Cimela <siphocimela@gmail.com>
2023-01-30 09:31:36 +0200

    learning

--- src/main/java/linkedin/courseone/threads/ThreadDemo.java
+++ src/main/java/linkedin/courseone/threads/ThreadDemo.java
@@ -1,37 +1,45 @@
 package linkedin.courseone.threads;
 
-class  Hi extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
-class Hello extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
 public class ThreadDemo {
     public static void main(String[] args) {
+        /**
+         *  Used anonymous classes
+         *  Used lambda expressions
+         * */
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t1.start();
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {
 
-        Hi obj = new Hi();
-        Hello obj2 = new Hello();
-        obj.start();
-        obj2.start();
+        }
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            
+        }
+
+        System.out.println("Bye");
     }
 }
